b90d17e9178402bc9a742b8c6a93bf7c8330550c
Yuan <rook1e404@outlook.com>
2025-11-28 14:14:07 +0800

    使用合并后的 ipv6 源
    Removed several IPv6 download links from the script.

--- build.sh
+++ build.sh
@@ -15,15 +15,8 @@ download() {
 download https://raw.githubusercontent.com/metowolf/iplist/master/data/special/china.txt
 download https://raw.githubusercontent.com/17mon/china_ip_list/master/china_ip_list.txt
 # IPv6
-download https://raw.githubusercontent.com/gaoyifan/china-operator-ip/refs/heads/ip-lists/cernet6.txt
 download https://raw.githubusercontent.com/gaoyifan/china-operator-ip/refs/heads/ip-lists/china6.txt
-download https://raw.githubusercontent.com/gaoyifan/china-operator-ip/refs/heads/ip-lists/chinanet6.txt
-download https://raw.githubusercontent.com/gaoyifan/china-operator-ip/refs/heads/ip-lists/cmcc6.txt
-download https://raw.githubusercontent.com/gaoyifan/china-operator-ip/refs/heads/ip-lists/cstnet6.txt
-download https://raw.githubusercontent.com/gaoyifan/china-operator-ip/refs/heads/ip-lists/drpeng6.txt
 download https://raw.githubusercontent.com/gaoyifan/china-operator-ip/refs/heads/ip-lists/googlecn6.txt
-download https://raw.githubusercontent.com/gaoyifan/china-operator-ip/refs/heads/ip-lists/tietong6.txt
-download https://raw.githubusercontent.com/gaoyifan/china-operator-ip/refs/heads/ip-lists/unicom6.txt
 
 mkdir -p dist
 
